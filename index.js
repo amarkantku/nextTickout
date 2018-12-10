68801610a22d19bff73c6a540c71cf306053422f
Amarkant Kumar <amar.du2013@gmail.com>
2018-12-10 10:31:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ Array.prototype.nextTickout = function(interval) {
   let i = 0;
   (function loop() {
     console.log(this[i]);
-    if (++i < this.length) {
-      setTimeout(loop.bind(this), interval);
-    }
+    if (this[++i]) {
+    	setTimeout(loop.bind(this), interval);
+    } 
   }.bind(this))();
 };
 
-[1, 2, 3, 4, 5].nextTickout(1000);
+[1, 2, 3, 4, , , , 5].filter(Boolean).nextTickout(1000);
