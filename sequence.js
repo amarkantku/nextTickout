045370b1484aca5ae4cd618651dbd702e6f7c23b
Amarkant Kumar <amar.du2013@gmail.com>
2020-06-08 21:40:45 +0530

    Create sequence.js

---
+++ sequence.js
@@ -0,0 +1,23 @@
+for (var i = 0; i < 3; i++) {
+  setTimeout(function(i){
+    console.log(i);
+  }.bind(this, i),1000);
+}
+
+
+for (var i = 0; i < 3; i++) {
+  (function(n){
+    console.log(n);
+  })(i);
+}
+
+
+for (var i = 0; i < 3; i++) {
+  (function(n){
+    setTimeout(function() {
+      console.log(n);
+    },1000);
+  })(i);
+}
+
+
